6252b17e0bd1c563e7ef3ebeca4b3fb788236772
Seungmin Yang <tmdals099@gmail.com>
2019-12-24 17:32:23 +0900

    스플래시 화면 생성

--- app/src/main/java/com/example/bcsd5/SplashActivity.java
+++ app/src/main/java/com/example/bcsd5/SplashActivity.java
@@ -1,14 +1,37 @@
 package com.example.bcsd5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                SplashActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        }).start();
     }
 }
